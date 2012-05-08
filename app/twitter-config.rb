d2fca3543173cb6c6ebde8f62e6e3030e2b19f29
ned rihine <ned.rihine@gmail.com>
2012-05-08 15:32:36 +0900

    ライセンスファイルとコンシューマキーなモジュールの書かれたファイルを追加。

---
+++ app/twitter-config.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+=begin
+  twitter-config.rb にはついったー用のコンシューマーキー用モジュールが入っています。
+
+  Copyright (C) 2012 ned rihine All rights reserved.
+=end
+
+module TwitterConsumer
+  CONSUMER_KEY = 'f0tDONnPe3998s4bIb0P5Q'
+  CONSUMER_SECRET = 'B5ecg0mapSqUfC5y6PQbHxtUqRZMBkln0RhFkhs'
+end
