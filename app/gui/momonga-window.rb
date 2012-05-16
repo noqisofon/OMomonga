d2bb598172a9ceacd07bc6f2e0813d11aef4be68
ned rihine <ned.rihine@gmail.com>
2012-05-16 21:43:39 +0900

    メイン窓にメニューを追加しました。

--- app/gui/momonga-window.rb
+++ app/gui/momonga-window.rb
@@ -21,12 +21,23 @@ module OMomonga::Gui
     private
     def init_widgets
       @vbox0 = Gtk::VBox.new( false, 0 )
+      @menu_bar = Gtk::MenuBar.new
       @tree_view = Gtk::TreeView.new
 
       #
       # @vbox0
       #
+      @vbox0.pack_start @menu_bar, false, false
       @vbox0.pack_start @tree_view
+
+      #
+      # @menu_bar
+      #
+      init_menu
+
+      #
+      # @tree_view
+      #
 
       #
       # self
@@ -34,6 +45,25 @@ module OMomonga::Gui
       set_size_request 320, 500
       set_border_width 5
       add @vbox0
+    end
+
+    def init_menu
+      { "ツイート(T)" => [ "新しいツイート(N)", "----", "終了(Q)" ] ,
+        "編集(E)" => [ "設定(P)" ],
+        "ヘルプ(H)" => [ "おばけももんがとは(A)" ] }.each do |k, v|
+        menu_strip_item = Gtk::MenuItem.new( k )
+        tool_strip_subitems = Gtk::Menu.new
+        v.each do |label|
+          unless label =~ /-+/ then
+            tool_strip_subitems.append Gtk::MenuItem.new( label )
+          else
+            tool_strip_subitems.append Gtk::SeparatorMenuItem.new
+          end
+        end
+        menu_strip_item.submenu = tool_strip_subitems
+
+        @menu_bar.append menu_strip_item
+      end
     end
   end
 
