90ed8c52139c6de21c84c692666e5e310420da27
ned rihine <ned.rihine@gmail.com>
2013-10-21 10:43:29 +0900

    Twitter オブジェクトを引数に取ったり取らなかったりするように修正

--- src/ghostly/momonga/twitter.clj
+++ src/ghostly/momonga/twitter.clj
@@ -5,53 +5,14 @@
                       TwitterException
                       TwitterFactory)
            (twitter4j.auth AccessToken
-                           RequestToken)))
+                           RequestToken))
+  (:require [clojure.java.io :as [io]]))
+
+
+(defstruct token :key :secret)
+
 
 (def *twitter* (TwitterFactory/getSingleton))
-
-
-(defn consumer-key-set! [consumer-key consumer-secret]
-  (.setOAuthConsumer *twitter* consumer-key consumer-secret))
-
-
-(defn request-token []
-  (.getOAuthRequestToken *twitter*))
-
-
-(defn authorization-url
-  ([]
-     (let [rtoken (request-token)]
-       (authorization-url rtoken)))
-  ([rtoken]
-     (.getAuthorizationURL rtoken)))
-
-
-(defn access-token [rtoken pin]
-  (let [atoken (.getOAuthAccessToken *twitter* rtoken pin)]
-    (store-access-token atoken)
-    atoken))
-
-
-(defn- store-access-token [atoken]
-  nil)
-
-
-(defn post [msg]
-  (.updateStatus *twitter* msg))
-
-
-(defn home-timeline
-  ([]
-     (.getHomeTimeline *twitter*))
-  ([pg]
-     (.getHomeTimeline *twitter* pg)))
-
-
-(defn friends-timeline
-  ([]
-     (.getFriendsTimeline *twitter*))
-  ([pg]
-     (.getFriendsTimeline *twitter* pg)))
 
 
 (defn paging [& {page :page cnt :count since-id :since-id max-id :max-id}]
@@ -65,6 +26,83 @@
    ((nil? max-id)
     (Paging. page cnt since-id))
    ((every? nil? [cnt max-id])
-    (Paging. page since-id)
-    (:else
-     (Paging. page cnt since-id max-id)))))
+    (Paging. page since-id))
+   (:else
+    (Paging. page cnt since-id max-id))))
+
+
+(defn consumer-key-set! [consumer-key consumer-secret]
+  (.setOAuthConsumer *twitter* consumer-key consumer-secret))
+
+
+(defn request-token
+  ([]
+     (request-token *twitter*))
+  ([a_twitter]
+     (.getOAuthRequestToken a_twitter))
+
+
+(defn authorization-url
+  ([]
+     (let [a_request-token (request-token)]
+       (authorization-url a_request-token)))
+  ([a_request-token]
+     (.getAuthorizationURL a_request-token)))
+
+
+(defn access-token
+  ([a_request-token pin]
+     (access-token *twitter* a_request-token pin))
+  ([a_twitter a_request-token pin]
+     (let [an_access-token (.getOAuthAccessToken a_twitter a_request-token pin)]
+       (store-access-token an_access-token)
+       an_access-token)))
+
+
+(defn- store-access-token [an_access-token]
+  (with-open [w (io/writer "twitter4j.properties" :append true)]
+    (.write (str "oauth.accessToken" "=" (.getToken an_access-token) "\n"))
+    (.write (str "oauth.accessTokenSecret" "=" (.getTokenSecret an_access-token) "\n"))))
+
+
+(defn update
+  ([update-message]
+     (update *twitter* update-message))
+  ([a_twitter update-message]
+     (.updateStatus a_twitter update-message)))
+
+
+(defn home-timeline
+  ([]
+     (home-timeline *twitter*))
+  ([^Twitter a_twitter]
+     (.getHomeTimeline a_twitter))
+  ([^Paging a_paging]
+     (home-timeline *twitter* a_paging))
+  ([^Twitter a_twitter ^Paging a_paging]
+     (.getHomeTimeline a_twitter a_paging)))
+
+
+(defn friends-timeline
+  ([]
+     (home-timeline *twitter*))
+  ([^Twitter a_twitter]
+     (.getFriendsTimeline a_twitter))
+  ([^Paging a_paging]
+     (home-timeline *twitter* a_paging))
+  ([^Twitter a_twitter ^Paging a_paging]
+     (.getFriendsTimeline a_twitter a_paging)))
+
+
+(defn menthions-timeline 
+  ([]
+     (home-timeline *twitter*))
+  ([^Twitter a_twitter]
+     (.getMenthionsTimeline a_twitter))
+  ([^Paging a_paging]
+     (home-timeline *twitter* a_paging))
+  ([^Twitter a_twitter ^Paging a_paging]
+     (.getMenthionsTimeline a_twitter a_paging)))
+
+
+;; (defn status [id])
